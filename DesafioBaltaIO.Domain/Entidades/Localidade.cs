80e34f4f56cf9f284f437a52c2f0b8b13abdd0db
pmfrp <pmfrp@hotmail.com>
2023-10-20 15:35:34 -0300

    LocalidadeModel

---
+++ DesafioBaltaIO.Domain/Entidades/Localidade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioBaltaIO.Domain.Entidades
+{
+    public class Localidade
+    {
+        public int Codigo { get; private set; }
+        public string Estado { get; private set; }
+        public string Cidade { get; private set; }
+    }
+}
---
+++ DesafioBaltaIO/Domain/Models/IBGE/Specs/LocalidadeSpecifications.cs
@@ -0,0 +1,46 @@
+﻿using NetDevPack.Specification;
+using System.Linq.Expressions;
+
+namespace DesafioBaltaIO.Domain.Models.IBGE.Specs
+{
+    public class LocalidadeEstadoNaoNuloOuVazioSpecification : Specification<LocalidadeModel>
+    {
+        public override Expression<Func<LocalidadeModel, bool>> ToExpression()
+        {
+            return localidade => localidade.Estado.Length == 2;
+        }
+    }
+
+    public class LocalidadeCidadeNaoNuloOuVazioSpecification : Specification<LocalidadeModel>
+    {
+        public override Expression<Func<LocalidadeModel, bool>> ToExpression()
+        {
+            return localidade => !string.IsNullOrEmpty(localidade.Cidade);
+        }
+    }
+
+    public class LocalidadeCodigoNaoNuloOuVazioSpecification : Specification<LocalidadeModel>
+    {
+        public override Expression<Func<LocalidadeModel, bool>> ToExpression()
+        {
+            return localidade => !string.IsNullOrEmpty(localidade.Codigo);
+        }
+    }
+
+    public class LocalidadeCodigoEhNumeroSpecification : Specification<LocalidadeModel>
+    {
+        public override Expression<Func<LocalidadeModel, bool>> ToExpression()
+        {
+            int result;
+            return localidade => int.TryParse(localidade.Codigo, out result);
+        }
+    }   
+    
+    public class LocalidadeCodigoNoPadraoSpecification : Specification<LocalidadeModel>
+    {
+        public override Expression<Func<LocalidadeModel, bool>> ToExpression()
+        {
+            return localidade => int.Parse(localidade.Codigo) >= 1000000;
+        }
+    }
+}
