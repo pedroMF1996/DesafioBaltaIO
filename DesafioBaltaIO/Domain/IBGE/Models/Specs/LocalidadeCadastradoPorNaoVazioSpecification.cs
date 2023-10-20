d58d21338edcae454cd9b6ccb8d7a4d7870b2d30
pmfrp <pmfrp@hotmail.com>
2023-10-20 17:08:47 -0300

    Reestruturacao dos arquivos spec

---
+++ DesafioBaltaIO/Domain/IBGE/Models/Specs/LocalidadeCadastradoPorNaoVazioSpecification.cs
@@ -0,0 +1,18 @@
+﻿using NetDevPack.Specification;
+using System.Linq.Expressions;
+
+namespace DesafioBaltaIO.Domain.IBGE.Models.Specs
+{
+    #endregion
+
+    #region CadastradoPor
+    public class LocalidadeCadastradoPorNaoVazioSpecification : Specification<LocalidadeModel>
+    {
+        public override Expression<Func<LocalidadeModel, bool>> ToExpression()
+        {
+            return localidade => Guid.Empty != localidade.CadastradoPor;
+        }
+    }
+    #endregion
+
+}
---
+++ DesafioBaltaIO/Domain/IBGE/Models/Specs/LocalidadeCodigoEhNumeroSpecification.cs
@@ -0,0 +1,19 @@
+﻿using NetDevPack.Specification;
+using System.Linq.Expressions;
+
+namespace DesafioBaltaIO.Domain.IBGE.Models.Specs
+{
+    #endregion
+    #region CodigoSpecifications
+
+    public class LocalidadeCodigoEhNumeroSpecification : Specification<LocalidadeModel>
+    {
+        public override Expression<Func<LocalidadeModel, bool>> ToExpression()
+        {
+            int result;
+            return localidade => int.TryParse(localidade.Codigo, out result);
+        }
+    }
+    #endregion
+
+}
