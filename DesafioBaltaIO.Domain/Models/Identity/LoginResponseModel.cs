8c1d5a5feff97a3397e230cd3ab294ead950c063
pmfrp <pmfrp@hotmail.com>
2023-10-20 14:06:08 -0300

    IdentityModels

---
+++ DesafioBaltaIO.Domain/Models/Identity/LoginResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DesafioBaltaIO.Domain.Models.Identity
+{
+
+    public class LoginResponseModel
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UserTokenModel UserToken { get; set; }
+    }
+}
