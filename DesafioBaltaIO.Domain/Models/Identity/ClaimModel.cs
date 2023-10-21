3ccefbefef0ddee1afe254a79378f33f97452d68
Pedro Martins Falleiros <32961778+pedroMF1996@users.noreply.github.com>
2023-10-20 22:07:34 -0300

    Delete DesafioBaltaIO.Domain directory

--- DesafioBaltaIO.Domain/Models/Identity/ClaimModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DesafioBaltaIO.Domain.Models.Identity
-{
-    public class ClaimModel
-    {
-        public string Value { get; set; }
-        public string Type { get; set; }
-    }
-}
