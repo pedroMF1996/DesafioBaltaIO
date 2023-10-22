5304767082e8a44ae9e59b19879dff4f7d3363b0
pmfrp <pmfrp@hotmail.com>
2023-10-21 23:48:21 -0300

    Fazendo a limpa

--- DesafioBaltaIO/Configurations/RegisterServiceConfiguration.cs
+++ DesafioBaltaIO/Configurations/RegisterServiceConfiguration.cs
@@ -1,6 +1,10 @@
-﻿using DesafioBaltaIO.Application.Autenticacao.mediatorHandler;
+﻿using DesafioBaltaIO.Application.Autenticacao.Commands;
+using DesafioBaltaIO.Application.Autenticacao.DTOs;
+using DesafioBaltaIO.Application.Autenticacao.mediatorHandler;
 using DesafioBaltaIO.Application.Ibge.Commands;
 using DesafioBaltaIO.Application.Ibge.Events;
+using DesafioBaltaIO.Application.Ibge.Query;
+using DesafioBaltaIO.Application.Ibge.Query.Interface;
 using DesafioBaltaIO.Data.Atenticacao;
 using DesafioBaltaIO.Data.IBGE;
 using DesafioBaltaIO.Data.IBGE.Repositories;
@@ -16,13 +20,23 @@ namespace DesafioBaltaIO.Configurations
     {
         public static IServiceCollection AddRegisterServices(this IServiceCollection services)
         {
+            #region API
+
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
             services.AddScoped<IMediatorHandler, MediatorHandler>();
             services.AddScoped<IMediatorHandlerAutenticacao, MediatorHandlerAutenticacao>();
 
+            #endregion
+
+            #region Events
+
             services.AddScoped<INotificationHandler<LocalidadeCadastradaEvent>, IbgeEventHandler>();
             services.AddScoped<INotificationHandler<LocalidadeEditadaEvent>, IbgeEventHandler>();
+
+            #endregion
+
+            #region Commands
 
             services.AddScoped<IRequestHandler<CadastrarLocalidadeCommand, ValidationResult>, IbgeCommandHandler>();
             services.AddScoped<IRequestHandler<AlterarCodigoLocalidadeCommand, ValidationResult>, IbgeCommandHandler>();
@@ -30,10 +44,24 @@ namespace DesafioBaltaIO.Configurations
             services.AddScoped<IRequestHandler<AlterarCidadeLocalidadeCommand, ValidationResult>, IbgeCommandHandler>();
             services.AddScoped<IRequestHandler<RemoverLocalidadeCommand, ValidationResult>, IbgeCommandHandler>();
 
+            services.AddScoped<IRequestHandler<RegistrarUsuarioCommand, AutenticarUsuarioResponseDTO>, AutenticacaoCommandHandler>();
+            services.AddScoped<IRequestHandler<AutenticarUsuarioCommand, AutenticarUsuarioResponseDTO>, AutenticacaoCommandHandler>();
+
+            #endregion
+
+            #region Queries
+
+            services.AddScoped<ILocalidadeQueries, LocalidadeQueries>();
+
+            #endregion
+
+            #region Datas
+
             services.AddScoped<IbgeDbContext>();
             services.AddScoped<ApplicationDbContext>();
             services.AddScoped<ILocalidadeReository, LocalidadeRepository>();
 
+            #endregion
             return services;
         }
     }
--- DesafioBaltaIO/Program.cs
+++ DesafioBaltaIO/Program.cs
@@ -15,17 +15,7 @@ var app = builder.Build();
 
 app.UseApiConfiguration(app.Environment);
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.AddIbgeController();
 app.AddAutenticacaoController();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
